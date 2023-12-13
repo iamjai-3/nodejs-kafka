b1184b6b0f7289075042001e88b0f20ede0cc63d
iamjai <jai.develop3r@gmail.com>
2023-12-13 10:37:22 +0530

    added more integration tests

--- catalog_service/src/api/__tests__/catalog.routes.test.ts
+++ catalog_service/src/api/__tests__/catalog.routes.test.ts
@@ -62,4 +62,98 @@ describe('Catalog Routes', () => {
       expect(response.body).toEqual('error occurred on create product');
     });
   });
+
+  describe('PATCH /products/:id', () => {
+    test('should update product successfully', async () => {
+      const product = ProductFactory.build();
+      const requestBody = {
+        name: product.name,
+        price: product.price,
+        stock: product.stock
+      };
+      jest
+        .spyOn(catalogService, 'updateProduct')
+        .mockImplementationOnce(() => Promise.resolve(product));
+      const response = await request(app)
+        .patch(`/products/${product.id}`)
+        .send(requestBody)
+        .set('Accept', 'application/json');
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(product);
+    });
+
+    test('should response with validation error 400', async () => {
+      const product = ProductFactory.build();
+      const requestBody = {
+        name: product.name,
+        price: -1,
+        stock: product.stock
+      };
+      const response = await request(app)
+        .patch(`/products/${product.id}`)
+        .send({ ...requestBody })
+        .set('Accept', 'application/json');
+      expect(response.status).toBe(400);
+      expect(response.body).toEqual('price must not be less than 1');
+    });
+
+    test('should response with an internal error code 500', async () => {
+      const product = ProductFactory.build();
+      const requestBody = mockRequest();
+      jest
+        .spyOn(catalogService, 'updateProduct')
+        .mockImplementationOnce(() =>
+          Promise.reject(new Error('unable to update product'))
+        );
+      const response = await request(app)
+        .patch(`/products/${product.id}`)
+        .send(requestBody)
+        .set('Accept', 'application/json');
+      expect(response.status).toBe(500);
+      expect(response.body).toEqual('unable to update product');
+    });
+  });
+
+  describe('GET /products?limit=0&offset=0', () => {
+    test('should return a range of products based on limit and offset', async () => {
+      const randomLimit = faker.number.int({ min: 10, max: 50 });
+      const products = ProductFactory.buildList(randomLimit);
+      jest
+        .spyOn(catalogService, 'getProducts')
+        .mockImplementationOnce(() => Promise.resolve(products));
+      const response = await request(app)
+        .get(`/products?limit=${randomLimit}&offset=0`)
+        .set('Accept', 'application/json');
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(products);
+    });
+  });
+
+  describe('GET /products/:id', () => {
+    test('should return a product by id', async () => {
+      const product = ProductFactory.build();
+      jest
+        .spyOn(catalogService, 'getProduct')
+        .mockImplementationOnce(() => Promise.resolve(product));
+      const response = await request(app)
+        .get(`/products/${product.id}`)
+        .set('Accept', 'application/json');
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(product);
+    });
+  });
+
+  describe('DELETE /products/:id', () => {
+    test('should delete a product by id', async () => {
+      const product = ProductFactory.build();
+      jest
+        .spyOn(catalogService, 'deleteProduct')
+        .mockImplementationOnce(() => Promise.resolve({ id: product.id }));
+      const response = await request(app)
+        .delete(`/products/${product.id}`)
+        .set('Accept', 'application/json');
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual({ id: product.id });
+    });
+  });
 });
--- catalog_service/src/api/catalog.routes.ts
+++ catalog_service/src/api/catalog.routes.ts
@@ -1,5 +1,5 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { CreateProductRequest } from '../dto/product.dto';
+import { CreateProductRequest, UpdateProductRequest } from '../dto/product.dto';
 import { CatalogRepository } from '../repository/catalog.repository';
 import { CatalogService } from '../services/catalog.service';
 import { RequestValidator } from '../utils/requestValidator';
@@ -33,13 +33,59 @@ router.patch(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { errors, input } = await RequestValidator(
-        CreateProductRequest,
+        UpdateProductRequest,
         req.body
       );
 
+      const id = parseInt(req.params.id) || 0;
+
       if (errors) return res.status(400).json(errors);
-      const data = await catalogService.createProduct(input);
-      return res.status(201).json(data);
+
+      const data = await catalogService.updateProduct({ id, ...input });
+      return res.status(200).json(data);
+    } catch (error) {
+      const err = error as Error;
+      return res.status(500).json(err.message);
+    }
+  }
+);
+
+router.get(
+  '/products',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const limit = Number(req.query['limit']);
+    const offset = Number(req.query['offset']);
+    try {
+      const data = await catalogService.getProducts(limit, offset);
+      return res.status(200).json(data);
+    } catch (error) {
+      const err = error as Error;
+      return res.status(500).json(err.message);
+    }
+  }
+);
+
+router.get(
+  '/products/:id',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = parseInt(req.params.id) || 0;
+    try {
+      const data = await catalogService.getProduct(id);
+      return res.status(200).json(data);
+    } catch (error) {
+      const err = error as Error;
+      return res.status(500).json(err.message);
+    }
+  }
+);
+
+router.delete(
+  '/products/:id',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = parseInt(req.params.id) || 0;
+    try {
+      const data = await catalogService.deleteProduct(id);
+      return res.status(200).json(data);
     } catch (error) {
       const err = error as Error;
       return res.status(500).json(err.message);
